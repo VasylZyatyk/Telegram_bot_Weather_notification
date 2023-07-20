706a32cccaa55fb5c2bc3c0213f958f87302b107
Yurii <135170315+YuriiZiatyk@users.noreply.github.com>
2023-07-20 17:02:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,60 +1,57 @@
-const telegramBot = require('node-telegram-bot-api');
+const Telegraf = require('telegraf');
 const axios = require('axios');
 require('dotenv').config();
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
-const bot = new telegramBot(token, { polling: true });
+const bot = new Telegraf(token);
 const weatherApiKey = process.env.OPENWEATHERMAP_API_KEY;
 
-bot.on('message', async (msg) => {
-    if (msg.text.startsWith('/weather')) {
-        const chatId = msg.chat.id;
-        const city = msg.text.split(' ')[1];
+bot.command('weather', async (ctx) => {
+    const city = ctx.message.text.split(' ')[1];
 
-        try {
-            const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}`);
-            const weatherData = response.data;
+    try {
+        const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}`);
+        const weatherData = response.data;
 
-            const kelvinToCelsius = (temp) => (temp - 273.15).toFixed(2);
-            const temperature = kelvinToCelsius(weatherData.main.temp);
-            const feelsLike = kelvinToCelsius(weatherData.main.feels_like);
-            const description = weatherData.weather[0].description;
+        const kelvinToCelsius = (temp) => (temp - 273.15).toFixed(2);
+        const temperature = kelvinToCelsius(weatherData.main.temp);
+        const feelsLike = kelvinToCelsius(weatherData.main.feels_like);
+        const description = weatherData.weather[0].description;
 
-            bot.sendMessage(chatId, `Температура в ${city} становить ${temperature} і відчувається як ${feelsLike}°C. Погода ${description}.`);
-        } catch (error) {
-            bot.sendMessage(chatId, 'Вибачте,я не отримав дані про погоду.Спробуйте пізніше.');
-        }
+        ctx.reply(`Температура в ${city} становить ${temperature} і відчувається як ${feelsLike}°C. Погода ${description}.`);
+    } catch (error) {
+        ctx.reply('Вибачте,я не отримав дані про погоду.Спробуйте пізніше.');
     }
 });
 
+const subscriptions = [];
 
-const subscriptions = {};
-
-bot.onText(/\/subscribe (.+)/, (msg, match) => {
-    const chatId = msg.chat.id;
-    const location = match[1];
+bot.command('subscribe', (ctx) => {
+    const chatId = ctx.chat.id;
+    const location = ctx.message.text.split(' ')[1];
 
     // Add the user's chat ID and location to the subscriptions object
     subscriptions[chatId] = location;
 
-    bot.sendMessage(chatId, `Ви успішно підписались на оновлення даних про погоду для ${location}.`);
+    ctx.reply(`Ви успішно підписались на оновлення даних про погоду для ${location}.`);
 });
 
-bot.onText(/\/unsubscribe/, (msg) => {
-    const chatId = msg.chat.id;
+bot.command('unsubscribe', (ctx) => {
+    const chatId = ctx.chat.id;
 
     // Remove the user's chat ID from the subscriptions object
     delete subscriptions[chatId];
 
-    bot.sendMessage(chatId, 'Ви успішно відписались від оновлень даних про погоду.');
+    ctx.reply('Ви успішно відписались від оновлень даних про погоду.');
 });
+
 // Function to send daily weather updates to all subscribed users
 const sendDailyUpdates = async () => {
     // Get the current date and time
     const now = new Date();
 
     // Calculate the time until the next update (9:00 AM local time)
-    const nextUpdate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 9, 0, 0, 0) - now;
+    let nextUpdate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 9, 0, 0, 0) - now;
     if (nextUpdate < 0) {
         nextUpdate += 86400000; // it's after 9am, try 9am tomorrow.
     }
@@ -73,12 +70,14 @@ const sendDailyUpdates = async () => {
             const feelsLike = kelvinToCelsius(weatherData.main.feels_like);
             const description = weatherData.weather[0].description;
 
-            bot.sendMessage(chatId, `Температура ${location} становить ${temperature}°C , відчувається як ${feelsLike}°C. Погода ${description}.`);
+            bot.telegram.sendMessage(chatId, `Температура ${location} становить ${temperature}°C , відчувається як ${feelsLike}°C. Погода ${description}.`);
         } catch (error) {
-            bot.sendMessage(chatId, 'Вибачте,я не отримав дані про погоду.Спробуйте пізніше');
+            bot.telegram.sendMessage(chatId, 'Вибачте,я не отримав дані про погоду.Спробуйте пізніше');
         }
     }
 };
 
 // Start sending daily updates
 sendDailyUpdates();
+
+bot.launch();
